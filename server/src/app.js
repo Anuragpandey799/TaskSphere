edd5c4e26c92cdbe40d65ab134e8367740f7f71b
Anurag pandey <anuhackerag799@gmail.com>
2026-02-19 13:07:54 +0530

    updated cors

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://task-sphere-vert.vercel.app",
+      "https://task-sphere-anurag.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
